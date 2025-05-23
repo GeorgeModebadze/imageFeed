e8eddf7694359dcb6e13d316ef9c3ca073c2a076
GeorgeModebadze <g.modebadze@kodington.ru>
2025-05-23 19:28:55 +0700

    sprint_08 complete

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -12,4 +12,7 @@ final class ImagesListCell: UITableViewCell {
 
     static let reuseIdentifier = "ImagesListCell"
     
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
 }
