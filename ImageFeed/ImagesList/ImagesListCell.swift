730b5f3428cde4136500e3d5f359c5093f3bbdd2
GeorgeModebadze <g.modebadze@kodington.ru>
2025-05-23 18:49:15 +0700

    ready for configure Images in cells

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Георгий on 23.05.2025.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
