3474660fa389c852de14c149d8dac59485843ba2
GeorgeModebadze <g.modebadze@kodington.ru>
2025-05-30 23:44:15 +0700

    added singleImageViewController

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Георгий on 30.05.2025.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
