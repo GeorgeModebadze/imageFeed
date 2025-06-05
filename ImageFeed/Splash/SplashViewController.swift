3ce5846976785d9e61901df4afb4f0d72282584d
GeorgeModebadze <g.modebadze@kodington.ru>
2025-06-05 15:03:01 +0700

    tryN2

--- ImageFeed/Splash/SplashViewController.swift
+++ ImageFeed/Splash/SplashViewController.swift
@@ -97,8 +97,15 @@ final class SplashViewController: UIViewController {
         present(alert, animated: true)
     }
     
+//    private func showAuthScreen() {
+//        let authViewController = AuthViewController()
+//        authViewController.delegate = self
+//        authViewController.modalPresentationStyle = .fullScreen
+//        present(authViewController, animated: true)
+//    }
     private func showAuthScreen() {
-        let authViewController = AuthViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let authViewController = storyboard.instantiateViewController(withIdentifier: "AuthViewController") as! AuthViewController
         authViewController.delegate = self
         authViewController.modalPresentationStyle = .fullScreen
         present(authViewController, animated: true)
