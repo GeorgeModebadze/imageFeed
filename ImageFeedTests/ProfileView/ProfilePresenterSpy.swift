24f7b92f0f5c54e1378b25fb80b77676d151cd28
GeorgeModebadze <g.modebadze@kodington.ru>
2025-06-12 07:37:54 +0700

    ProfileViewTest with errors

--- ImageFeedTests/ProfileView/ProfilePresenterSpy.swift
+++ ImageFeedTests/ProfileView/ProfilePresenterSpy.swift
@@ -1,33 +1,21 @@
-//@testable import ImageFeed
-//
-//final class ProfilePresenterSpy: ProfilePresenterProtocol {
-//    weak var view: ProfileViewControllerProtocol?
-//    
-//    private(set) var didCallViewDidLoad = false
-//    private(set) var didCallDidUpdateAvatar = false
-//    private(set) var didCallShowLogoutConfirmationAlert = false
-//    private(set) var didCallPerformLogout = false
-//    private(set) var didCallExitProfile = false
-//    
-//    
-//    func viewDidLoad() {
-//        didCallViewDidLoad = true
-//    }
-//    
-//    func didUpdateAvatar() {
-//        didCallDidUpdateAvatar = true
-//    }
-//    
-//    func showLogoutConfirmationAlert() {
-//        didCallShowLogoutConfirmationAlert = true
-//        view?.showLogoutConfirmationAlert()
-//    }
-//    
-//    func performLogout() {
-//        didCallPerformLogout = true
-//    }
-//    
-//    func exitProfile() {
-//        didCallExitProfile = true
-//    }
-//}
+import Foundation
+@testable import ImageFeed
+
+final class ProfilePresenterSpy: ProfilePresenterProtocol {
+    var view: ProfileViewControllerProtocol?
+    var viewDidLoadCalled = false
+    var didUpdateAvatarCalled = false
+    var performLogoutCalled = false
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateAvatar() {
+        didUpdateAvatarCalled = true
+    }
+    
+    func performLogout() {
+        performLogoutCalled = true
+    }
+}
--- ImageFeedTests/ProfileView/ProfileTests.swift
+++ ImageFeedTests/ProfileView/ProfileTests.swift
@@ -1,6 +1,75 @@
 import XCTest
 @testable import ImageFeed
 
-class ProfileViewControllerTests: XCTestCase {
-  
+final class ProfileViewTests: XCTestCase {
+    
+    func testViewControllerCallsViewDidLoad() {
+        // given
+        let viewController = ProfileViewController()
+        let presenter = ProfilePresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+    
+    func testPresenterCallsUpdateProfileDetails() {
+        // given
+        let viewController = ProfileViewControllerSpy()
+        let presenter = ProfilePresenter()
+        presenter.view = viewController
+        
+        // when
+        presenter.viewDidLoad()
+        
+        // then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertTrue(viewController.updateProfileDetailsCalled)
+        }
+    }
+    
+    func testPresenterCallsUpdateAvatar() {
+        // given
+        let viewController = ProfileViewControllerSpy()
+        let presenter = ProfilePresenter()
+        presenter.view = viewController
+        
+        // when
+        presenter.didUpdateAvatar()
+        
+        // then
+        XCTAssertTrue(viewController.updateAvatarCalled)
+    }
+    
+    func testLogoutButtonCallsConfirmationAlert() {
+        // given
+        let viewController = ProfileViewController()
+        let presenter = ProfilePresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        _ = viewController.view // чтобы вызвать viewDidLoad
+        viewController.didTapButton() // имитируем нажатие кнопки
+        
+        // then
+        XCTAssertTrue(presenter.performLogoutCalled)
+    }
+    
+    func testLogoutCallsSwitchToAuth() {
+        // given
+        let viewController = ProfileViewControllerSpy()
+        let presenter = ProfilePresenter()
+        presenter.view = viewController
+        
+        // when
+        presenter.performLogout()
+        
+        // then
+        XCTAssertTrue(viewController.switchToAuthViewControllerCalled)
+    }
 }
